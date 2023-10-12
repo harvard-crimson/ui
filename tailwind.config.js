36748140491f2513186fa614ba4b634f6de9a1c4
nhshah15 <neilhemang@gmail.com>
2023-10-12 19:15:02 -0400

    Updated colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'crimson': '#a82931',
+      'dark-crimson': '#8c252e',
+      'black': '#000000',
+      'dark-gray': '#808285',
+      'gray': '#bcbec0',
+      'light-gray': '#e6e7e8',
+      'multi': '#004e6a',
+      'dark-multi': '#003b55',
+      'metro': '#4daae9',
+      'darker-metro': '#208cd7',
+      'arts': '#f99d1c',
+      'dark-arts': '#f37e0d',
+      'sports': '#218446',
+      'dark-sports': '#1c5f36'
+    },
     extend: {},
   },
   plugins: [],
