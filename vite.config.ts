53fcc35eb698fb41865bae16329f0a0b9c5f1a11
dseum22 <dseum22@gmail.com>
2023-11-09 16:06:06 -0500

    Refactoring path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@lib': resolve(__dirname, './lib'),
+      '@src': resolve(__dirname, './src'),
+    },
+  },
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
